25b2988cece55b8ea343f8ebf52246bedcc369a3
fatima03598 <54324041+fatima03598@users.noreply.github.com>
2020-05-14 10:18:05 +0100

    table created

--- db/migrations/20200514095834_create-recipes-table.js
+++ db/migrations/20200514095834_create-recipes-table.js
@@ -1,8 +1,17 @@
 
-exports.up = function(knex) {
-  
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('recipes', (table) => {
+      table.increments();
+      table.string('title').notNullable();
+      table.text('ingridients').notNullable();
+      table.string('difficulty').notNullable();
+      table.integer('minutes').notNullable();
+      table.integer('serving').notNullable();
+      table.string('imageURL').notNullable();
+      table.text('method', longtext).notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('recipes');
 };
