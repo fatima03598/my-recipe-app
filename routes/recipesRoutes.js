d26713e1670e455559f99ebf453e8f921a0bda22
fatima03598 <54324041+fatima03598@users.noreply.github.com>
2020-05-14 12:18:29 +0100

    completed routing

--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -11,12 +11,52 @@ router.get('/recipes', (req, res, next) => {
     .catch(err => next(err))
 });
 
-router.get('/recipes/')
+router.get('/recipes/:id', (req, res, next) => {
+    queries.getSingle(req.params.id)
+    .then((recipe) => {
+        res.status(200).json(recipe);
+    })
+    .catch(err => next(err))
+});
 
+router.post('/recipes', (req, res, next) => {
+    queries.add(req.body)
+    .then((recipeID) => {
+        return queries.getSingle(recipeID)
+    })
+    .then((recipe) => {
+        res.json(recipe);
+    })
+    .catch(err => next(err))
+});
 
+router.put('/recipes/:id', (req, res, next) => {
+    if(req.body.hasOwnProperty('id')) {
+        return res.status(422).json({
+            error: 'Id cannot be updated'
+            
+        });
+    }
+    queries.update(req.params.id, req.body)
+    .then( () => {
+        return queries.getSingle(req.params.id);
+    })
+    .then((recipe) => {
+        res.status(200).json(recipe);
+    })
+    .catch(err => next(err))
+});
 
-
-
+router.delete('/recipes/:id', (req, res, next) => {
+    queries.getSingle(req.params.id)
+    .then((recipe) => {
+        queries.deleteItem(req.params.id)
+        .then(() => res.status(200).json(recipe))
+        .catch(err => next(err))
+    })
+    .catch(err =>next(err))
+    
+})
 
 
 
